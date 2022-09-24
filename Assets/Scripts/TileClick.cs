ee6f6b6a862f912a4ab3ba638cf4b3cf5c1edcaa
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-09-24 23:11:48 +0200

    added UI pointer logic

--- Assets/Scripts/TileClick.cs
+++ Assets/Scripts/TileClick.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
-public class TileClick : MonoBehaviour
+using UnityEngine.EventSystems;
+
+public class TileClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
    private SpriteRenderer _sr;
-   private void Awake() => _sr = gameObject.GetComponent<SpriteRenderer>();
+   private GridManager _gridManager;
+   private float _dimmedOpacity;
 
+   private void Awake()
+   {
+      _sr = gameObject.GetComponent<SpriteRenderer>();
+      _gridManager = FindObjectOfType<GridManager>();
+      _dimmedOpacity = _gridManager.UnselectedOpacity;
+   }
 
    private void OnMouseDown() => _sr.flipY = true;
 
    private void OnMouseUp() => _sr.flipY = false;
+
+   public void OnPointerDown(PointerEventData eventData)
+   {
+      _gridManager.UpdateSelectedTile(gameObject, 1);
+   }
+
+   public void OnPointerUp(PointerEventData eventData)
+   {
+      _gridManager.UpdateSelectedTile(gameObject, _dimmedOpacity);
+   }
 }
