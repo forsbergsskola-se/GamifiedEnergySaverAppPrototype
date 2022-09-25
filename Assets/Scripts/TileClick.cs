8bb13089d215cf52deb6aab2c0d2ec1f8f04a502
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-09-26 00:08:37 +0200

    Removed old TileClick logic

--- Assets/Scripts/TileClick.cs
+++ Assets/Scripts/TileClick.cs
@@ -3,27 +3,18 @@ using UnityEngine.EventSystems;
 
 public class TileClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-   //private SpriteRenderer _sr;
    private GridManager _gridManager;
    [SerializeField] public int Index;
 
-   private void Awake()
-   {
-      //_sr = gameObject.GetComponent<SpriteRenderer>();
+   
+   private void Awake() =>
       _gridManager = FindObjectOfType<GridManager>();
-   }
 
-   //private void OnMouseDown() => _sr.flipY = true;
-
-   //private void OnMouseUp() => _sr.flipY = false;
-
+   
    public void OnPointerDown(PointerEventData eventData)
-   {
-      _gridManager.SelectTile(1, Index);
-   }
+      => _gridManager.SelectTile(1, Index);
+   
 
-   public void OnPointerUp(PointerEventData eventData)
-   {
+   public void OnPointerUp(PointerEventData eventData) =>
       _gridManager.DropTile();
-   }
 }
