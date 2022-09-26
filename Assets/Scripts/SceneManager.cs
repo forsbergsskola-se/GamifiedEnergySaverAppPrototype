d536b6608c43d6f562d23f05862d1f41188a484a
jackahlberg <ahlberg.jack@gmail.com>
2022-09-26 17:34:04 +0100

    WIP connects scenes

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,6 +6,7 @@ public class SceneManager : MonoBehaviour
 
     public void OpenScene()
     {
+        Debug.Log("Changing Scene");
         UnityEngine.SceneManagement.SceneManager.LoadScene(index);
     }
 }
