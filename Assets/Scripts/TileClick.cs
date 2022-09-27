5ed7bc25285197be6838ba5d597c125362c86f32
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-09-27 20:32:27 +0200

    Camera doesn't drag if placing buildings

--- Assets/Scripts/TileClick.cs
+++ Assets/Scripts/TileClick.cs
@@ -4,17 +4,28 @@ using UnityEngine.EventSystems;
 public class TileClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
    private GridManager _gridManager;
+   private CameraDrag _cameraDrag;
    [SerializeField] public int Index;
 
-   
-   private void Awake() =>
+
+   private void Awake()
+   {
       _gridManager = FindObjectOfType<GridManager>();
+      _cameraDrag = FindObjectOfType<CameraDrag>();
+   }
 
 
-   public void OnPointerDown(PointerEventData eventData) =>
+   public void OnPointerDown(PointerEventData eventData)
+   {
+      _cameraDrag.canMove = false;
       _gridManager.SelectTile(gameObject,1, Index);
+   }
 
 
-   public void OnPointerUp(PointerEventData eventData) =>
+   public void OnPointerUp(PointerEventData eventData)
+   {
+      _cameraDrag.canMove = true;
       _gridManager.DropTile();
+   }
+      
 }
