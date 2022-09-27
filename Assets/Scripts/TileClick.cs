f72b9c2058f5315db4938fd500c573106462fa18
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-09-27 14:48:46 +0200

    Update TileClick.cs

--- Assets/Scripts/TileClick.cs
+++ Assets/Scripts/TileClick.cs
@@ -10,10 +10,10 @@ public class TileClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
    private void Awake() =>
       _gridManager = FindObjectOfType<GridManager>();
 
-   
-   public void OnPointerDown(PointerEventData eventData)
-      => _gridManager.SelectTile(1, Index);
-   
+
+   public void OnPointerDown(PointerEventData eventData) =>
+      _gridManager.SelectTile(gameObject,1, Index);
+
 
    public void OnPointerUp(PointerEventData eventData) =>
       _gridManager.DropTile();
